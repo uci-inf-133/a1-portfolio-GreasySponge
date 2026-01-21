21a7310f7c82eb62c22e4df9965648c028ce6b21
GreasySponge <112441845+GreasySponge@users.noreply.github.com>
2026-01-21 22:35:55 +0000

    Fixed unity game implementation and added personal image.

--- index.js
+++ index.js
@@ -22,3 +22,31 @@ function openTab (evt, tabName) {
     }
 
 }
+
+var container = document.querySelector("#unity-container");
+var canvas = document.querySelector("#unity-canvas");
+var loadingBar = document.querySelector("#unity-loading-bar");
+var playOverlay = document.querySelector("#play-overlay");
+var loadButton = document.querySelector("#load-game-btn");
+
+var buildUrl = "Build";
+var loaderUrl = buildUrl + "/a1-portfolio-GreasySponge.loader.js";
+var config = {
+    dataUrl: buildUrl + "/a1-portfolio-GreasySponge.data.br",
+    frameworkUrl: buildUrl + "/a1-portfolio-GreasySponge.framework.js.br",
+    codeUrl: buildUrl + "/a1-portfolio-GreasySponge.wasm.br",
+    streamingAssetsUrl: "StreamingAssets",
+    companyName: "DefaultCompany",
+    productName: "DormRoomOne",
+    productVersion: "0.1.0",
+};
+
+window.addEventListener('DOMContentLoaded', () => {
+    const loadButton = document.querySelector("#play-overlay button");
+
+    if (loadButton) {
+        loadButton.onclick = () => {
+            loadUnityGame();
+        };
+    }
+});
